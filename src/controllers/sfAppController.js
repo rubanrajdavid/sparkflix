4ccd298d56099cc25ca2ebe492bc422b7c090051
Ruban Raj David <rajruban.d@gmail.com>
2020-09-15 22:35:48 +0530

    Experiment with Read Stream & Chunk Send Function

--- src/controllers/sfAppController.js
+++ src/controllers/sfAppController.js
@@ -1,4 +1,6 @@
 const bodyParser = require("body-parser");
+const fs = require('fs')
+const path = require('path')
 const moviesModel = require('../models/moviesModel')
 const screensModel = require('../models/screensModel')
 
@@ -49,6 +51,42 @@ let controller = {
             })
         })
     },
+    sampleRender: (req, res) => {
+        res.render("sparkflix/sample.handlebars")
+    },
+    sample: (req, res) => {
+        const path = 'src/views/public/assets/videos/'+req.params.mID+'.mp4'
+        const stat = fs.statSync(path)
+        const fileSize = stat.size
+        const range = req.headers.range
+
+        if (range) {
+            const parts = range.replace(/bytes=/, "").split("-")
+            const start = parseInt(parts[0], 10)
+            const end = parts[1]
+                ? parseInt(parts[1], 10)
+                : fileSize - 1
+
+            const chunksize = (end - start) + 1
+            const file = fs.createReadStream(path, { start, end })
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            }
+
+            res.writeHead(206, head)
+            file.pipe(res)
+        } else {
+            const head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            }
+            res.writeHead(200, head)
+            fs.createReadStream(path).pipe(res)
+        }
+    }
 }
 
 module.exports = controller
--- src/routes/sfAppRouter.js
+++ src/routes/sfAppRouter.js
@@ -6,6 +6,9 @@ router.get("/", controller.moviesPageRender);
 router.get("/createscreen", controller.createScreenPageRender);
 router.get("/screens", controller.screensPageRender);
 
+router.get("/sample", controller.sampleRender)
+router.get("/samplevideo/:mID", controller.sample)
+
 router.get("/movie/:mID", controller.movieStream);
 
 
